885066ab8af3059d9a44cfaaca2e0f5e9a897dec
Reed A. Cartwright <reed@cartwrig.ht>
2009-11-05 22:41:04 +0000

    return the ability to read sequences from stdin on '-'

--- src/seqdb.cpp
+++ src/seqdb.cpp
@@ -17,6 +17,8 @@
 
 #include "ngila.h"
 
+#include <cstring>
+#include <string>
 #include <algorithm>
 #include <boost/bind.hpp>
 
@@ -30,17 +32,29 @@ bool seq_db::parse_file(const char *csfile, bool bappend, bool bi, int dirs)
 {
 	if(!bappend)
 		clear();
-	
-	file_iterator<char> file_first(csfile);
-	if(!file_first)
-		return CERRORR("unable to open \'" << csfile << "\'");
-	file_iterator<char>  file_last = file_first.make_end();
-		
+
 	stack<string> my_stack;
 	seq_grammar my_grammar(my_stack, *this);
-	parse_info< file_iterator<char> > info = parse(file_first, file_last, my_grammar);
-	if (!info.full)
-		return CERRORR("unable to parse \'" << csfile << "\'");
+	
+	if(strcmp(csfile, "-")==0) {
+		string ss; 
+		getline(cin, ss, '\004');
+		if(ss.empty())
+			return CERRORR("unable to open stdin.");
+
+		parse_info<string::const_iterator> info = parse(ss.begin(), ss.end(), my_grammar);
+		if (!info.full)
+			return CERRORR("unable to parse stdin.");
+	} else {
+		file_iterator<char> file_first(csfile);
+		if(!file_first)
+			return CERRORR("unable to open \'" << csfile << "\'");
+		file_iterator<char>  file_last = file_first.make_end();
+		
+		parse_info< file_iterator<char> > info = parse(file_first, file_last, my_grammar);
+		if (!info.full)
+			return CERRORR("unable to parse \'" << csfile << "\'");
+	}
 		
 	for(container::iterator it = cont.begin(); it != cont.end(); ++it)
 		cont.modify(it, bind(&seq_data::finalize, _1, bi, dirs));
